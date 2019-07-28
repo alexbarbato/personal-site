5aeea9fa0c94ae759ed82e38e683e917447482ec
Alex Barbato <alex.j.barbato@gmail.com>
2019-07-27 22:54:15 -0400

    Update package json for vulnerabilities and a few minor text updates

--- src/pages/credits.js
+++ src/pages/credits.js
@@ -4,7 +4,7 @@ import { Layout } from "../components/organisms/layout"
 export default () => (
   <Layout activePage="Credits">
     <h1 style={{ fontFamily: "Permanent Marker" }}>
-      There are few that deserve credit
+      There are a few that deserve credit
     </h1>
   </Layout>
 )
--- src/pages/work.js
+++ src/pages/work.js
@@ -3,6 +3,6 @@ import { Layout } from "../components/organisms/layout"
 
 export default () => (
   <Layout activePage="Work">
-    <h1 style={{ fontFamily: "Permanent Marker" }}>A bit about me</h1>
+    <h1 style={{ fontFamily: "Permanent Marker" }}>A bit about my work</h1>
   </Layout>
 )
