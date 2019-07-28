be0db072b4c14948b64739d2e3de9f57a1cda2d0
Alex Barbato <alex.j.barbato@gmail.com>
2019-07-27 23:08:56 -0400

    Beginning to add a base for the credits page

--- src/pages/credits.js
+++ src/pages/credits.js
@@ -1,10 +1,16 @@
 import React from "react"
 import { Layout } from "../components/organisms/layout"
+import Image from "../components/atoms/image"
 
 export default () => (
-  <Layout activePage="Credits">
+  <Layout activePage="Credits" column>
     <h1 style={{ fontFamily: "Permanent Marker" }}>
       There are a few that deserve credit
     </h1>
+    <section>
+      <h2>Kirsten</h2>
+      <Image imgsrc="kirsten.png" />
+      <p />
+    </section>
   </Layout>
 )
